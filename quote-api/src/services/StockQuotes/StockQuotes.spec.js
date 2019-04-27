33ad24b89d04875974e508cb63ddf7efc1a4d60a
Christopher Rizzo <christophermatthewrizzo@gmail.com>
2019-04-27 18:06:41 -0400

    StockQuote service - init IEX api routes

---
+++ quote-api/src/services/StockQuotes/StockQuotes.spec.js
@@ -0,0 +1,19 @@
+import StockQuotes from '.';
+
+StockQuotes.getSymbols()
+    .then( (symbols) => {
+        console.log('symbols: ', symbols.length);
+    })
+    .catch( err => console.error(err) );
+
+StockQuotes.getQuote('AAPL')
+    .then((quote) => {
+        console.log('Quote: ', quote);
+    })
+    .catch( err => console.error(err) );
+
+StockQuotes.getChart('AAPL')
+    .then( (quote) => {
+        console.log('Sample Chart data: ', quote[0]);
+    })
+    .catch( err => console.error(err) );

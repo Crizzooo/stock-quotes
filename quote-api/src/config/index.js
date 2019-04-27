7450a8a417ec124cd91fda61533f55c51eded1fa
Christopher Rizzo <christophermatthewrizzo@gmail.com>
2019-04-27 16:37:49 -0400

    server - init config with path info

---
+++ quote-api/src/config/index.js
@@ -0,0 +1,4 @@
+import path from 'path';
+
+export const FRONT_END_DIST_PATH = path.join(__dirname, '..', '..', 'quote-ui', 'build');
+export const FRONT_END_ENTRY_PATH = path.join(FRONT_END_DIST_PATH, 'index.html');

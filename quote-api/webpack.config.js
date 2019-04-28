956c660e368c9355037352f43213468447d567cf
Christopher Rizzo <christophermatthewrizzo@gmail.com>
2019-04-27 21:36:25 -0400

    server webpack - add source maps for development purposes

--- quote-api/webpack.config.js
+++ quote-api/webpack.config.js
@@ -1,32 +1,32 @@
 const path = require('path');
 
 module.exports = {
-  entry: './src/app.js',
-  watch: process.env.WATCH_SERVER ? true : false,
-  module: {
-    rules: [
-      {
-        test: /\.js$/,
-        exclude: /(node_modules|bower_components)/,
-        use: {
-          loader: 'babel-loader',
-          options: {
-            presets: ['@babel/preset-env']
-          }
-        }
-      }
-    ]
-  },
-  node: {
-    // Allow __dirname to follow nnormal Node.js behavior rather than be fixed to "/"
-    // https://webpack.js.org/configuration/node/#node-__dirname
-    __dirname: false,
-  },
-  target: 'node',
-  devtool: 'source-map',
-  output: {
-    filename: 'app.js',
-    libraryTarget: 'commonjs2',
-    path: path.resolve(__dirname, 'dist'),
-  },
+	entry: './src/app.js',
+	watch: process.env.WATCH_SERVER ? true : false,
+	module: {
+		rules: [
+			{
+				test: /\.js$/,
+				exclude: /(node_modules|bower_components)/,
+				use: {
+					loader: 'babel-loader',
+					options: {
+						presets: ['@babel/preset-env']
+					}
+				}
+			}
+		]
+	},
+	node: {
+		// Allow __dirname to follow nnormal Node.js behavior rather than be fixed to "/"
+		// https://webpack.js.org/configuration/node/#node-__dirname
+		__dirname: false,
+	},
+	target: 'node',
+	devtool: 'source-map',
+	output: {
+		filename: 'app.js',
+		libraryTarget: 'commonjs2',
+		path: path.resolve(__dirname, 'dist'),
+	},
 }

a00e98e1bf8e04a7d389c752756f66bd23693e88
Christopher Rizzo <christophermatthewrizzo@gmail.com>
2019-04-28 21:37:44 -0400

    centralize quote data cache time to config

--- quote-api/src/config/index.js
+++ quote-api/src/config/index.js
@@ -2,3 +2,4 @@ import path from 'path';
 
 export const FRONT_END_DIST_PATH = path.join(__dirname, '..', '..', 'quote-ui', 'build');
 export const FRONT_END_ENTRY_PATH = path.join(FRONT_END_DIST_PATH, 'index.html');
+export const QUOTE_DATA_CACHE_TIME = 60000 * 5;

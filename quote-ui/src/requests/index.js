9fdc4ff588fd2ea65ad180b72a55ea7b5f5b0e02
Christopher Rizzo <christophermatthewrizzo@gmail.com>
2019-04-28 21:32:25 -0400

    add get quote request

--- quote-ui/src/requests/index.js
+++ quote-ui/src/requests/index.js
@@ -3,3 +3,18 @@ export function getSearchResults(searchVal) {
     return fetch(`${searchRoute}`)
             .then( (res) => res.json() );
 }
+
+export function getQuote(symbol) {
+    if (!symbol) {
+        throw new Error('Symbol String not provided to getQuote!');
+    }
+    const searchRoute = `/api/stocks/quotes/${symbol}`;
+    return fetch(`${searchRoute}`)
+            .then( (res) => res.json() )
+            .then( ({ error, data }) => {
+                if (error) {
+                    throw new Error(`Could not fetch ${symbol}`);
+                }
+                return data;
+            });
+}
